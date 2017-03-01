cecff8d0a0a70de74f531c996af592bd054405cd
root <root@debian>
2017-03-01 13:13:50 +0100

    planets.xml update

---
+++ eje1.py
@@ -0,0 +1,13 @@
+from lxml import etree
+
+doc=etree.parse('planets.xml')
+
+raiz = doc.getroot()
+
+planets = raiz.findall('planet')
+
+print len(planets)
+print raiz.find("name")
+
+for planet in planets:
+    print planet.find("name").text
